5bf73d2245324b31b46865e3cced74459923e8d7
Essam Sayed <essam.sayed01@gmail.com>
2019-08-04 15:24:03 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
     
   devServer: {
     host: '0.0.0.0',
-    port: '8080',
+    port: '3000',
     hot: true,
-    public: '165.22.212.127:8080'
+    public: '165.22.212.127:3000'
   
   }
 }
