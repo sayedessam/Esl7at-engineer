bc00243fa84d9c16399b43eda46f72e942657139
Essam Mustafa Sayed <essam.sayed01@gmail.com>
2019-08-04 12:16:03 +0000

    ignore server config

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
     
   devServer: {
     host: '0.0.0.0',
-    port: '8086',
+    port: '8080',
     hot: true,
-    public: '165.22.212.127:8086'
+    public: '165.22.212.127:8080'
   
   }
 }
