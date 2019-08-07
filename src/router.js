cfab2da7c93d6d7516cf5b02a1536bcca269a000
unknown <essam.sayed01@gmail.com>
2019-08-07 08:20:19 +0300

    inspection changes not finished

--- src/router.js
+++ src/router.js
@@ -66,6 +66,14 @@ export default new Router({
 			// this generates a separate chunk (about.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () => import(/* webpackChunkName: "appointmentlist" */ './views/appointment/AppointmentList.vue')
+		},
+		{
+			path: '/addappointment',
+			name: 'addappointment',
+			// route level code-splitting
+			// this generates a separate chunk (about.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import(/* webpackChunkName: "appointmentlist" */ './views/appointment/AddAppointment.vue')
 		}
 	]
 });
